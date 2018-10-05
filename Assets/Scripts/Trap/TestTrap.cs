26a117a8088504155c4fe78959ef6cb61ca6b23f
Andrew Batdorf <abatdorf@purdue.edu>
2018-10-05 16:56:29 -0400

    Trap types implemented

--- Assets/Scripts/Trap/TestTrap.cs
+++ Assets/Scripts/Trap/TestTrap.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class TestTrap : GameTile {
 	private bool sprung;
+    public string trapType;
     override protected void Awake() {
         base.Awake();
         this.sprung = false;
     }
     public override void SetCharacter(Character character) {	
-        base.SetCharacter(character);
-        if(character.IsPlayer() && this.sprung == false){
+        if(this.sprung == false && this.character != character){
+            base.SetCharacter(character);
         	Debug.Log("Trap time~");
-        	this.sprung = true;
+            switch (trapType)
+            {
+                case "Damage":
+                Debug.Log("Damage Trap");
+                character.ReceiveDamage(200);
+                this.sprung = true;
+                break; 
+                case "Resetting":
+                Debug.Log("Resetting Trap");
+                character.ReceiveDamage(100);
+                break;
+                case "Move":
+                Debug.Log("Move Trap");
+                Vector2 movement = Vector2.left; 
+                new MovementAction(character, character.GetCoordinates() + movement, character.movementSpeed ,false);
+                this.sprung = true;
+                break;
+                default:
+                Debug.Log("Invalid Trap Type");
+                break;
+            }
         }
-
-
     }
 }
