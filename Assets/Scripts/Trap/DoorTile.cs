b763013e5b31a80d967b5ba6389c5d33a3a87df1
Andrew Batdorf <abatdorf@purdue.edu>
2018-10-24 14:59:24 -0400

    DoorTile File created

---
+++ Assets/Scripts/Trap/DoorTile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTile : GameTile {
+
+	private GameManager = curGm;
+	override protected void Awake() {
+		base.Awake();
+		this.isWalkable = false;
+		curGm = FindObjectOfType<GameManager>();	
+	}
+
+}
