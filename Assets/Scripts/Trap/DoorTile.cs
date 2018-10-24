c711204ab398ca78c1fd5a01f13f4589bb9f1ee6
Andrew Batdorf <abatdorf@purdue.edu>
2018-10-24 15:35:32 -0400

    Doors without keys implemented

--- Assets/Scripts/Trap/DoorTile.cs
+++ Assets/Scripts/Trap/DoorTile.cs
@@ -4,11 +4,38 @@ using UnityEngine;
 
 public class DoorTile : GameTile {
 
-	private GameManager = curGm;
+	private GameManager curGm;
+	private Player curPlayer;
+	public Sprite openSprite;
+	private bool isOpen;
+	public bool isLockedDoor = false;
+
 	override protected void Awake() {
 		base.Awake();
 		this.isWalkable = false;
+		this.isOpen = false;
 		curGm = FindObjectOfType<GameManager>();	
 	}
 
+	private void OnMouseDown() {
+		this.curPlayer = curGm.GetPlayer();
+		if((this.GetCoordinates() - curPlayer.GetCoordinates()).magnitude == 1 && this.isOpen == false){
+			if(this.isLockedDoor){
+				Debug.Log("Locked!");
+				this.OpenDoor();
+			}else{
+				Debug.Log("Not Locked!");
+				this.OpenDoor();
+			}
+		}
+
+	}
+
+	private void OpenDoor(){
+		this.isWalkable = true;
+		SpriteRenderer renderer = this.GetComponent<SpriteRenderer>();
+		renderer.sprite = openSprite;
+		this.isOpen = true;
+		Debug.Log("Hello");
+	}
 }
