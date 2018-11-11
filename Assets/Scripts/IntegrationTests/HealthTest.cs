13f0cbc83180bf32759ace21742a0d8ebda8128d
07734willy <piortau@gmail.com>
2018-11-10 21:41:59 -0500

    basic integration testing working

---
+++ Assets/Scripts/IntegrationTests/HealthTest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTest : IntegrationTest {
+    public override string description {
+        get {
+            return "health was not decremented";
+        }
+    }
+
+    public override IEnumerator Run() {
+        this.status = TestManager.Status.running;
+
+        Player player = GameObject.Find("Player").GetComponent<Player>();
+        int health = player.health;
+        player.ReceiveDamage(200);
+
+        if (player.health > health) {
+            this.status = TestManager.Status.passed;
+        } else {
+            this.status = TestManager.Status.failed;
+        }
+        yield return null;
+    }
+}
